ce94426295f9b2dfdb2ff0e1058a90519e10922f
murilosrodrigues <murilorodrigues29@gmail.com>
2024-07-09 08:01:09 -0300

    Add project files.

---
+++ src/Journey.Infrastructure/JourneyDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Journey.Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Journey.Infrastructure;
+
+public class JourneyDbContext : DbContext
+{
+    public DbSet<Trip> Trips { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=C:\\Users\\muril\\OneDrive\\Documentos\\projetos\\Rocketseat\\Journey\\JourneyDatabase.db");
+
+    }
+
+
+}
